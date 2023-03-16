5bb761bdb9171e6033504bc3c83eb9eb1d265077
sayurifranco <lsshiroma@gmail.com>
2023-03-16 14:35:20 +0000

    Exemplo cálculo desconto JS

---
+++ Declaracao-variaveis/calculo_desconto.js
@@ -0,0 +1,9 @@
+
+let salario = 4000;
+let bonus = 0.2;
+let descontoINSS = 0.11;
+
+let salarioBruto = salario * bonus;
+let salarioLiquido = salarioBruto * descontoINSS;
+
+console.log(salarioLiquido);
