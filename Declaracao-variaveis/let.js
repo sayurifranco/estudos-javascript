25a5bffc008118b4c9fd9cc9f19b8669439f646f
sayurifranco <lsshiroma@gmail.com>
2023-03-16 14:26:11 +0000

    Adição de explicação declaração let JS

--- Declaracao-variaveis/let.js
+++ Declaracao-variaveis/let.js
@@ -4,3 +4,14 @@
 
  area = b * h;
  console.log(area);
+
+ // let [identificador] [=valor | expressão]
+
+ /* let tipoNumber = 12;
+    let tipoString1 = 'Texto entre aspas simples';
+    let tipoString2 = "Texto entre aspas duplas";
+    let tipoNull = null;
+    let tipoUndefined = undefined;
+    let tipoObject = { nome : 'José da Silva', idade : 18};
+    let tipoBoolean = true || false;
+ */
