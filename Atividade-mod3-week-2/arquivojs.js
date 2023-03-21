18e1c8221bbf65fe6050924eb6fc3a51362377f0
sayurifranco <lsshiroma@gmail.com>
2023-03-21 15:08:06 +0000

    Adição de arquivo html com JS incorporado

---
+++ Atividade-mod3-week-2/arquivojs.js
@@ -0,0 +1,11 @@
+function clique_botao () {
+
+    var a,b;
+
+    a = 5;
+    b = 5;
+
+    var resultado = a+b;
+
+    alert("Você clicou no botão e a+b é igual a " + resultado)
+}
--- Atividade-mod3-week-2/src/main.js
+++ Atividade-mod3-week-2/src/main.js
@@ -1,10 +1,18 @@
-const leiaEntrada = require("readline");
+// const leiaEntrada = require("readline");
 
-const lendo = leiaEntrada.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
+// const lendo = leiaEntrada.createInterface({
+//     input: process.stdin,
+//     output: process.stdout,
+// });
 
-lendo.question("Qual o seu nome?", function (answer){
-    console.log(`Oh, seu nome é ${answer}`);
-});
+// lendo.question("Qual o seu nome?", function (answer){
+//     console.log(`Oh, seu nome é ${answer}`);
+// });
+// 
+
+var a,b;
+
+for (let i = 1; i <= 10; i++){
+    resultado = i * 2;
+        console.log(`Tabuada do 2 = ` + `2X` + i + ` = ` + resultado);
+}
