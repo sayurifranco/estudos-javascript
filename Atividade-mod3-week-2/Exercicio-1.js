9d527ad5a5cd9254722db565129132b82d79c3bd
sayurifranco <lsshiroma@gmail.com>
2023-03-21 17:53:40 +0000

    Exercicio array usando for, foreach while do-while

--- Atividade-mod3-week-2/Exercicio-1.js
+++ Atividade-mod3-week-2/Exercicio-1.js
@@ -2,3 +2,32 @@
 // usando cada uma das estruturas de repetição: 
 // while, do-while, for e foreach.
 
+const array = [15, 20, 14, 8, 5, 13, 4, 7, 2, 3]
+
+// let i = 0;
+
+// while (i < array.length) {
+//     console.log(array[i]);
+//     i++;
+// }
+
+// do {
+//     console.log(array[i]);
+//     i++;
+// } while (i < array.length);
+
+// for(let e = 0; e<array.length; e++){
+//     console.log(array[e])
+// };
+
+// array.forEach(e => console.log(e));
+
+// for (const e in array) {
+//     console.log(e); 
+// } --- imprime o índice da array
+
+for (const e of array) {
+    console.log(e);
+}
+
+// imprime os valores da array
